d817155f98324e6c18ae6d6f99f24a224459ee7d
SebasMen <sebastian.meneses@antpack.com>
2023-05-20 14:33:54 -0500

    [add] - view product detail ok@

--- js/product.js
+++ js/product.js
@@ -3,6 +3,7 @@ const inputSubstract = document.querySelector("#substract");
 const inputAdd = document.querySelector("#add");
 
 let quantity = inputSubstract.addEventListener("click", () => {
+  if (parseInt(inputQuantity.value) === 0) return;
   inputQuantity.value--;
 });
 
